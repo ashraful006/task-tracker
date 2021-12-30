324fe6b1dd6352a5e6abba764600944bd77c5503
ashraful006 <ashraful.p314159@gmail.com>
2021-12-31 01:05:05 +0600

    created post component and httpclient is used to get post data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { UserService } from './services/user.service';
+import { PostService } from './services/post.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PostsComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [UserService, PostService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
